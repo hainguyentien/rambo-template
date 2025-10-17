c65eb67d9326808ddf7cd7768a760a27151349b0
trong13012001 <tronga36192@gmail.com>
2025-10-17 10:19:32 +0700

    chore: integrated expo notification

--- scripts/reset-project.js
+++ scripts/reset-project.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 #!/usr/bin/env node
 
 /**
@@ -29,56 +33,4 @@ export default function Index() {
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
-}
-`;
-
-const layoutContent = `import { Stack } from "expo-router";
-
-export default function RootLayout() {
-  return <Stack />;
-}
-`;
-
-const moveDirectories = async () => {
-  try {
-    // Create the app-example directory
-    await fs.promises.mkdir(newDirPath, { recursive: true });
-    console.log(`📁 /${newDir} directory created.`);
-
-    // Move old directories to new app-example directory
-    for (const dir of oldDirs) {
-      const oldDirPath = path.join(root, dir);
-      const newDirPath = path.join(root, newDir, dir);
-      if (fs.existsSync(oldDirPath)) {
-        await fs.promises.rename(oldDirPath, newDirPath);
-        console.log(`➡️ /${dir} moved to /${newDir}/${dir}.`);
-      } else {
-        console.log(`➡️ /${dir} does not exist, skipping.`);
-      }
-    }
-
-    // Create new /app directory
-    const newAppDirPath = path.join(root, newAppDir);
-    await fs.promises.mkdir(newAppDirPath, { recursive: true });
-    console.log('\n📁 New /app directory created.');
-
-    // Create index.tsx
-    const indexPath = path.join(newAppDirPath, 'index.tsx');
-    await fs.promises.writeFile(indexPath, indexContent);
-    console.log('📄 app/index.tsx created.');
-
-    // Create _layout.tsx
-    const layoutPath = path.join(newAppDirPath, '_layout.tsx');
-    await fs.promises.writeFile(layoutPath, layoutContent);
-    console.log('📄 app/_layout.tsx created.');
-
-    console.log('\n✅ Project reset complete. Next steps:');
-    console.log(
-      "1. Run `npx expo start` to start a development server.\n2. Edit app/index.tsx to edit the main screen.\n3. Delete the /app-example directory when you're done referencing it."
-    );
-  } catch (error) {
-    console.error(`Error during script execution: ${error}`);
-  }
 };
-
-moveDirectories();
